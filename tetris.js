dae03f7734c2363f4ee2ae6d8e397a8dc3ea7071
Tapio.Teras <tapio.teras@siili.com>
2016-02-01 19:53:31 +0200

    added funky functionality

--- tetris.js
+++ tetris.js
@@ -1,6 +1,6 @@
-var width = 40;
+var width = 30;
 var height = 30;
-var velocity = 200;
+var velocity = 100;
 var output = '';
 var drawCompleted = false;
 var targetY = 0;
@@ -17,9 +17,9 @@ var Positions = {
 // #
 // ##
 var Brick = function(basePosition, drawBase, brickBody) {
-  var position = Positions.default;
-  var base = {x: 0, y: 0};
-  var body = [
+  var _position = Positions.default;
+  var _base = {x: 0, y: 0};
+  var _body = [
     {x: 0, y: 0}, 
     {x: 0, y: -1}, 
     {x: 1, y: 0}, 
@@ -27,23 +27,38 @@ var Brick = function(basePosition, drawBase, brickBody) {
   ];
   
   if (basePosition) {
-    position = basePosition;
+    _position = basePosition;
   }
-  if (body) {
-     body = brickBody;
+  if (brickBody) {
+     _body = brickBody;
   }
   
   return {
-    position: position,
-    base: base,
-    body: body
+    position: _position,
+    base: _base,
+    body: _body
   }
 };
+
+var shouldPrintBrick = function(brick, x, y) {
+  var draw = false;
+  for (var i = 0; brick.body.length < i; i++) {
+    var bX = brick.body[i].x;
+    var bY = brick.body[i].y;
+    bX = +sum(x)(bX);
+    bY = +sum(y)(bY);
+  }
+  if (x == bX || y == bY || (!y && bX) || (!x && !bY) ) {
+    return true;
+  }
+
+  return false;
+}; 
 
 var printFrame = function() {
   output = '';
   drawCompleted = false;
-  var targetX = 20;
+  var targetX = parseInt(height / 2);
   var objectToDraw = {
     position: Positions.default,
     base: {x: 0, y: 0},
@@ -54,23 +69,23 @@ var printFrame = function() {
       {x: 0, y: -2}
     ]
   };
-  
   for (var i = 0; i <= height; i++) {
     for (var j = 0; j <= width; j++) {
-      if (targetY == height) {
+      if (targetY > height) {
         // Start over
         targetY = 0;
-      }
-      else if (j == targetX && 
+        // TODO refactor duplicate code
+        output += '<span style="color: red;">&#9783;</span>';
+      } else if ((j == targetX && 
           i == targetY && 
-          !drawCompleted)
+          !drawCompleted) || shouldPrintBrick(new Brick()))
       {
         // Draw brick state
-        output += '_';
+        output += '<span style="color: blue;">&#9776;</span>';
         drawCompleted = true;
         targetY++;
       } else {
-        output += '#';
+        output += '<span style="color: red;">&#9783;</span>';
       }
     }
     output += '<br/>';
