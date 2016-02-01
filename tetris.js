8b0391ea4630c6149c1cb87243fc8d0e69611fc3
Tapio.Teras <tapio.teras@siili.com>
2016-02-01 19:17:12 +0200

    added logic for drawing tetris bricks

--- tetris.js
+++ tetris.js
@@ -5,43 +5,89 @@ var output = '';
 var drawCompleted = false;
 var targetY = 0;
 
+var Positions = {
+  n: 0,
+  e: 1,
+  s: 2,
+  w: 3,
+  default: 0
+}
+
 // #
 // #
 // ##
-var obj1 = {
-	base: 0, [
-  	{x: 0 y: 0}, 
-    {x: 0 y: -1}, 
-    {x: 1 y: 0}, 
-    {x: 0 y: -2}
-  ]
+var Brick = function(basePosition, drawBase, brickBody) {
+  var position = Positions.default;
+  var base = {x: 0, y: 0};
+  var body = [
+    {x: 0, y: 0}, 
+    {x: 0, y: -1}, 
+    {x: 1, y: 0}, 
+    {x: 0, y: -2}
+  ];
+  
+  if (basePosition) {
+    position = basePosition;
+  }
+  if (body) {
+     body = brickBody;
+  }
+  
+  return {
+    position: position,
+    base: base,
+    body: body
+  }
 };
 
-setInterval(function() {
-	output = '';
-	drawCompleted = false;
+var printFrame = function() {
+  output = '';
+  drawCompleted = false;
   var targetX = 20;
-	
+  var objectToDraw = {
+    position: Positions.default,
+    base: {x: 0, y: 0},
+    body: [
+      {x: 0, y: 0}, 
+      {x: 0, y: -1}, 
+      {x: 1, y: 0}, 
+      {x: 0, y: -2}
+    ]
+  };
+  
   for (var i = 0; i <= height; i++) {
-  	for (var j = 0; j <= width; j++) {
-    	if (targetY == height) {
-      	targetY = 0;
+    for (var j = 0; j <= width; j++) {
+      if (targetY == height) {
+        // Start over
+        targetY = 0;
       }
       else if (j == targetX && 
-      		i == targetY && 
+          i == targetY && 
           !drawCompleted)
       {
-      	output += '_';
+        // Draw brick state
+        output += '_';
         drawCompleted = true;
         targetY++;
       } else {
-      	output += '#';
+        output += '#';
       }
     }
-		output += '<br/>';
+    output += '<br/>';
   }
 document.body.innerHTML = output;
 i++;
-}, velocity);
+};
 
+var renderLoop = function(printCnt) {
+  var worldState = {
+    objects: []
+  };
+  setInterval(printCnt, velocity);
+}(printFrame);
 
+document.body.addEventListener("load", renderLoop);
+document.body.addEventListener('keydown', function(e) {
+    // todo: bind arrow keys!
+    //msg.textContent = 'keydown:' + e.keyCode;
+});
