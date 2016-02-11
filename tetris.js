1f4f08f77929e10d77639d12552088b3db0ef693
Tapio Teras <tapio.teras@siili.com>
2016-02-11 23:06:44 +0200

    enhanced brick drawing functionality - not completed yet!

--- tetris.js
+++ tetris.js
@@ -36,39 +36,40 @@ var Brick = function(basePosition, drawBase, brickBody) {
   return {
     position: _position,
     base: _base,
-    body: _body
+    body: _body,
+    moveDown: function() {
+      _base.y = _base.y - 1;
+    },
+    moveLeft: function() {
+      _base.x = _base.x - 1;
+    },
+    moveRight: function() {
+      _base.x = _base.x + 1;
+    }
   }
 };
 
-var shouldPrintBrick = function(brick, x, y) {
-  var draw = false;
-  for (var i = 0; brick.body.length < i; i++) {
-    var bX = brick.body[i].x;
-    var bY = brick.body[i].y;
-    bX = +sum(x)(bX);
-    bY = +sum(y)(bY);
-  }
-  if (x == bX || y == bY || (!y && bX) || (!x && !bY) ) {
-    return true;
-  }
+var brickRepo = [new Brick()];
 
-  return false;
-}; 
+var shouldDrawBrick = function(x, y, brickToDraw) {
+  var base = brickToDraw.base;
+  for (var i = 0; i < brickToDraw.body.length; i++) {
+    var relativeX = brickToDraw.body[i].x + base.x;
+    var relativeY = brickToDraw.body[i].y + base.y;
+    if (y === relativeY) {
+      if (x === relativeX) {
+        return true;
+      }
+    }
+  }
+  return base.x === x && base.y === y;
+};
 
 var printFrame = function() {
   output = '';
   drawCompleted = false;
   var targetX = parseInt(height / 2);
-  var objectToDraw = {
-    position: Positions.default,
-    base: {x: 0, y: 0},
-    body: [
-      {x: 0, y: 0}, 
-      {x: 0, y: -1}, 
-      {x: 1, y: 0}, 
-      {x: 0, y: -2}
-    ]
-  };
+  
   for (var i = 0; i <= height; i++) {
     for (var j = 0; j <= width; j++) {
       if (targetY > height) {
@@ -78,7 +79,7 @@ var printFrame = function() {
         output += '<span style="color: red;">&#9783;</span>';
       } else if ((j == targetX && 
           i == targetY && 
-          !drawCompleted) || shouldPrintBrick(new Brick()))
+          !drawCompleted) || shouldDrawBrick(j, i, new Brick()))
       {
         // Draw brick state
         output += '<span style="color: blue;">&#9776;</span>';
